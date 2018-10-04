015d3d5ac7141e49f15d11d0e90a0e31287ec761
Andres Faraone <andresfaraone@outlook.com>
2018-10-04 16:18:34 +0100

    added api weather functionality

--- thermostatapp.js
+++ thermostatapp.js
@@ -32,5 +32,24 @@ $( document ).ready(function(){
     $("#power-saving-status").text("power saving mode is " + thermostat.display())
   })
 
+  $('#submit').click(function() {
+    var city = $('#city-name').val()
+    $.get(get_url(city), function(data){
+      $("#result").text("The temperature in " + city + " is " + data.main.temp)
+    })
+  })
+
+  var api_key = '7c813b9bb506919b819c947817f24c6d'
+
+  var url_beginning = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+  var url_end = '&?id=524901&appid=' + api_key + '&units=metric'
+
+
+  var get_url = function(city) {
+    return url_beginning + city + url_end
+  }
+
+
 
 })
